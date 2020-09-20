63e82fd4895f98ef5557a3e3c11f4c252ede46aa
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-20 04:18:54 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
@@ -19,3 +20,26 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+=======
+import React, { Component } from 'react';
+import MovieCard from './card-components/MovieCard.js'
+import movieData from './data.js'
+
+export default class MovieShowcase extends Component {
+
+  generateMovieCards = () => {
+    // map over your movieData array and return an array of the correct JSX
+    return movieData.map(item => <li>{item.title}<li>
+    <li>{item.IMDBRating}<li>
+    )
+  }
+
+  render() {
+    return (
+      <div id="movie-showcase">
+        {this.generateMovieCards()}
+      </div>
+    )
+  }
+}
+>>>>>>> 2b1311e2ab9f74ceeeb1a0399c0c282529bb0124
